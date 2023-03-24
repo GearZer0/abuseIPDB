37df0bc72b04126d86b2389fb6df5fbac1aa49f6
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-03-24 12:29:01 +0800

    Update abuseipdb.py

--- abuseipdb.py
+++ abuseipdb.py
@@ -1,11 +1,23 @@
 import csv
 import requests
+import itertools
 
-API_KEY = '3dee6de9fb2a565ba8e8fa3972480b9e254e319f87b1f73da3a22e3ff5c2c245ca2037cd1882e8b2'  # Replace with your AbuseIPDB API key
+API_KEYS = [
+    'key1',
+    'key2',
+    'key3',
+    'key4',
+    'key5',
+    'key6',
+    'key7',
+    'key8',
+    'key9',
+    'key10'
+]  # Replace with your AbuseIPDB API keys
 
-def get_abuseipdb_info(ip):
+def get_abuseipdb_info(ip, api_key):
     url = f'https://api.abuseipdb.com/api/v2/check?ipAddress={ip}&verbose'
-    headers = {'Key': API_KEY, 'Accept': 'application/json'}
+    headers = {'Key': api_key, 'Accept': 'application/json'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()['data']
@@ -20,12 +32,14 @@ def get_abuseipdb_info(ip):
 if __name__ == '__main__':
     input_file = 'input.csv'
     output_file = 'output.csv'
+    keys_cycle = itertools.cycle(API_KEYS)
     with open(input_file, 'r') as csv_file, open(output_file, 'w', newline='') as output_csv:
         reader = csv.reader(csv_file)
         writer = csv.writer(output_csv)
         writer.writerow(['IP Address', 'Verdict', 'Resolved Domain', 'Hostname', 'Country'])
         for row in reader:
             ip = row[0]
-            verdict, resolved_domain, hostname, country = get_abuseipdb_info(ip)
+            api_key = next(keys_cycle)
+            verdict, resolved_domain, hostname, country = get_abuseipdb_info(ip, api_key)
             writer.writerow([ip, verdict, resolved_domain, hostname, country])
             print(f'IP: {ip}, Verdict: {verdict}, Resolved Domain: {resolved_domain}, Hostname: {hostname}, Country: {country}')
